bec680263e51b76f20aed27307b489edde643af3
ksh88boy <ksh88boy@naver.com>
2026-06-30 13:35:31 +0900

    feat: Form 1day

--- index.js
+++ index.js
@@ -38,3 +38,58 @@ console.log("Type empty", typeof empty);
 console.log("Type empty2", typeof empty2);
 console.log("Type userAge", typeof userAge);
 
+// 함수 
+// 함수 정의 
+function greeting() {
+  // 로직 
+  // 목표: "???님, 반갑습니다" 문자열을 콘솔에 찍기
+
+  // 1. 사용자 이름 담을 변수 선언  
+  // 2. 사용자에게 이름 받아와서 변수에 담기 
+  const inputUserName = window.prompt("이름을 입력해주세요.");
+  console.log("🚀 ~ greeting ~ inputUserName:", inputUserName);
+
+
+  // 3. "변수 + 님, 반갑습니다" 문자열 완성하기 
+  const result = `${inputUserName} 님, 반갑습니다`;
+  // 4. 완성된 문자열 콘솔에 찍기 
+  console.log(result);
+
+}
+
+// greeting();
+
+const greetingInput = (name) => {
+  console.log(`${name}님, 반갑습니다`);
+};
+
+greetingInput("후츠릿");
+greetingInput("김상호");
+greetingInput("노태호");
+greetingInput("이재욱");
+
+const add = (num1, num2) => {
+  console.log("🚀 ~ add ~ num1:", num1);
+  console.log("🚀 ~ add ~ num2:", num2);
+  return num1 + num2;
+}
+const doubleSum = (num) => {
+  return num * 2;
+}
+
+const addResult = add(10,30);
+const result = doubleSum(addResult);
+console.log("🚀 ~ result:", result);
+
+// 미션 : 소문자의 텍스트를 받아와서 대문자로 변환한 값을 반환하는 함수 만들기 
+
+const UpperFn = (str1) => {
+  return str1.toUpperCase();
+}
+const LowerFn = (str) => str.toLowerCase();
+
+const upperResult = UpperFn("kim");
+console.log("🚀 ~ upperResult:", upperResult)
+
+const lowerResult = LowerFn("KIM");
+console.log("🚀 ~ lowerResult:", lowerResult)
