68ef5594610aa87f7f0ba72fd3b447e6be1198b0
ksh88boy <ksh88boy@naver.com>
2026-06-30 10:37:28 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+//  주석
+console.log("멀티캠퍼스 강의중..");
+
+//  변수
+
+const userID = "chutzrit";
+console.log("userId",userID);
+
+let userName = "김탕도";
+console.log("userName", userName);
+
+//미션 :
+//적절한 변수명에 여러분의 이름을 담아서 변수 만들기
+//console에 찍어서 확인하기
+
+const userName2 = "김상호";
+
+userName = "kkkkk";
+console.log("userName2", userName2);
+
+// 데이터 타입 
+const userAge = 33; // 숫자 
+const userMBTI = "ENTJ"; //문자열
+console.log("🚀 ~ userMBTI:", userMBTI);
+
+// boolean타입
+const isLoggedin = true;
+console.log("🚀 ~ isLoggedin:", isLoggedin);
+console.log("Type isLoggedin", typeof isLoggedin);
+
+// null/undefined
+const empty = null;
+let empty2;
+console.log("🚀 ~ empty2:", empty2)
+
+// 미션: 나머지 변수들도 타입확인하는 코드 작성해서 콘솔에 찍어보기 
+console.log("Type empty", typeof empty);
+console.log("Type empty2", typeof empty2);
+console.log("Type userAge", typeof userAge);
+
